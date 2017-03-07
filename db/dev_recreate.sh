66bbf7b99deeba15d828edbd4d9c3c0171996cd2
Sergei Mellow <sergei.mellow@gmail.com>
2017-03-07 00:34:03 -0500

    configured hello world using template

---
+++ db/dev_recreate.sh
@@ -0,0 +1,6 @@
+RAILS_ENV=development bundle exec rake db:drop       
+RAILS_ENV=development bundle exec rake db:create     
+RAILS_ENV=development bundle exec rake db:schema:load
+RAILS_ENV=development bundle exec rake db:migrate    
+RAILS_ENV=development bundle exec rake db:seed       
+
