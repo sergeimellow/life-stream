1984c58fc9fd95cf7cc9dba6efc048c3e4e13c02
Sergei Mellow <sergei.mellow@gmail.com>
2017-04-25 23:59:49 -0400

    devise fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "<no-reply@#{SMTP_DOMAIN}>"
+  config.mailer_sender = "<no-reply@#{ENV.fetch("SMTP_DOMAIN")}>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
